9c858985e5a6b165f8c5fec50fa97f0bd38f3ea0
mayankpatel1004 <mayank.patel104@gmail.com>
2026-04-10 08:33:17 +0530

    Updates

--- src/helpers/logs.js
+++ src/helpers/logs.js
@@ -5,18 +5,18 @@ function logToFile(message, flag, url = '') {
   let filePath = '';
 
   if (flag === 'success') {
-    filePath = path.join(__dirname, '../../src/logs/logs_success.txt');
+    filePath = path.join(__dirname, '../../src/log/logs_success.txt');
     fs.appendFileSync(filePath, `${message}\n`);
 
   } else if (flag === 'request') {
-    filePath = path.join(__dirname, '../../src/logs/logs_request.json');
+    filePath = path.join(__dirname, '../../src/log/logs_request.json');
     const print_url = { url: url };
 
     fs.appendFileSync(filePath, JSON.stringify(print_url) + '\n');
     fs.appendFileSync(filePath, JSON.stringify(message) + '\n');
 
   } else {
-    filePath = path.join(__dirname, '../../src/logs/logs_fail.json');
+    filePath = path.join(__dirname, '../../src/log/logs_fail.json');
     fs.appendFileSync(filePath, `${message}\n`);
   }
 }
